cdb24e2cf473d511e091a59fba2a9359f438db4d
Tom Chen <tc3363@drexel.edu>
2025-03-12 20:59:13 -0400

    Setting variables for alert header in JS

---
+++ javascript/postload.js
@@ -0,0 +1,5 @@
+// Getting date information for the alert header
+
+deliveryDate = document.getElementById('delivery-date');
+orderDay = document.getElementById('order-day');
+orderDate = document.getElementById('order-date');
