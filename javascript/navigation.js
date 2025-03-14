a57911c8ef7b9f034d125f1b69802f5371496602
Tom Chen <tc3363@drexel.edu>
2025-03-14 03:21:29 -0400

    Create the navigation script file

---
+++ javascript/navigation.js
@@ -0,0 +1,6 @@
+// Click the buttons in the nav to show and fold different sections.
+
+menuBtnNav = document.getElementById('menu-btn-nav');
+deliveryBtnNav = document.getElementById('delivery-btn-nav');
+faqBtnNav = document.getElementById('faq-btn-nav');
+aboutBtnNav = document.getElementById('about-btn-nav');
