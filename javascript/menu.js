082b9fc409feb19db07f24f09b4c37484f794a26
Tom Chen <tc3363@drexel.edu>
2025-03-19 23:09:06 -0400

    Get menu data from website

---
+++ javascript/menu.js
@@ -0,0 +1,24 @@
+// Menu data collection
+
+// [name, price]
+
+const menu = [
+    ['Chorizo Breakfast Scramble', 13.50], 
+    ['Morning Hash', 13.00], 
+    ['Peanut Butter & Chocolate Chip Protein Pancakes', 12.00], 
+    ['Plain Protein Pancakes', 12.00], 
+    ['Egg Ground Turkey Breakfast Burrito', 13.00], 
+    ['Milan Salad', 12.00], 
+    ['Buffalo Chicken Wrap', 12.00], 
+    ['Chicken Salad Wrap', 12.00], 
+    ['Philly Chicken Cheesesteak', 12.50], 
+    ['Philly Beef Steak Sandwich', 14.00], 
+    ['Chicken Thigh Dirty Rice', 12.50], 
+    ['Custom Shrimp Box', 13.00], 
+    ['Wild for Salmon', 14.50], 
+    ['Deconstructed Chicken Pot Pie', 13.50], 
+    ['Carribean Jerk Salmon', 15.00], 
+    ['Grilled Shrimp Fettucini Alfredo', 14.50], 
+    ['Blackened Salmon', 15.00], 
+    ['Japanese Steak', 18.00]
+];
